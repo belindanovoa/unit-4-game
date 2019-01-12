3728c9352a98f106df90f1fb6a6aa2f2ea22a936
belindanovoa <novoa.belinda@gmail.com>
2019-01-11 19:16:39 -0800

    Final

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-   // scores
-   var playerTotal= 0; 
+ // scores
+   var userTotal= 0; 
    var wins= 0;
    var losses = 0;
+
+// Generates random number to guess
+  var Random=Math.floor(Math.random() * 102 + 19);
   
-  // Generates random number to guess
-  var Random=Math.floor(Math.random()*102+19);
-  
-  //Generate random number for each crystal
-  var num1= Math.floor(Math.random()*12+1);
-  var num2= Math.floor(Math.random()*12+1);
-  var num3= Math.floor(Math.random()*12+1);
-  var num4= Math.floor(Math.random()*12+1);
+//Generate random number for each fruit
+  var num1= Math.floor(Math.random() * 12 + 1);
+  var num2= Math.floor(Math.random() * 12 + 1);
+  var num3= Math.floor(Math.random() * 12 + 1);
+  var num4= Math.floor(Math.random() * 12 + 1);
+
+//random number
+$('.randomScore').text(Random);
 
 // Reset game
 function reset(){
-      Random=Math.floor(Math.random()*102+19);
-      $('#randomScore').text(Random);
-      num1= Math.floor(Math.random()*12+1);
-      num2= Math.floor(Math.random()*12+1);
-      num3= Math.floor(Math.random()*12+1);
-      num4= Math.floor(Math.random()*12+1);
-      playerTotal= 0;
-      $('#totalScore').text(playerTotal);
-      } 
+  Random=Math.floor(Math.random() * 102 + 19);
+  $('.randomScore').text(Random);
+  num1= Math.floor(Math.random() * 12 + 1 );
+  num2= Math.floor(Math.random() * 12 + 1);
+  num3= Math.floor(Math.random() * 12 + 1);
+  num4= Math.floor(Math.random() * 12 + 1);
+  userTotal= 0;
+  $('#totalScore').text(userTotal);
+  } 
 
+// Scoreboard
 $(document).ready(function(){
   $('#wins').text(wins);
   $('#losses').text(losses);
 
-//random number
-  $('#randomScore').text(Random);
-
 //wins
-function woohoo(){
-alert("You win! Have some fruit.");
+function winner(){
+alert("You win! Blessed Be the Fruit...");
   wins++; 
   $('#wins').text(wins);
   reset();
@@ -45,50 +46,49 @@ alert ("No fruit for you!");
   $('#losses').text(losses);
   reset();
 }
-// Click events that dont work
-  $('.strawberry').on ('click', function(){
-    playerTotal = playerTotal + num1;
-    console.log("New playerTotal= " + playerTotal);
-    $('#totalScore').text(playerTotal); 
-        if (playerTotal == Random){
-          woohoo();
+// Click events
+$('.strawberry').on ('click', function(){
+    userTotal = userTotal + num1;
+    console.log("New userTotal= " + userTotal);
+    $('#totalScore').text(userTotal); 
+        if (userTotal == Random){
+          winner();
         }
-        else if ( playerTotal > Random){
+        else if ( userTotal > Random){
           loser();
         }   
   })  
-  $('.lemon').on ('click', function(){
-    playerTotal = playerTotal + num2;
-    console.log("New playerTotal= " + playerTotal);
-    $('#totalScore').text(playerTotal); 
-        if (playerTotal == Random){
-          woohoo();
+$('.raspberry').on ('click', function(){
+  userTotal = userTotal + num2;
+    console.log("New userTotal= " + userTotal);
+    $('#totalScore').text(userTotal); 
+        if (userTotal == Random){
+          winner();
         }
-        else if ( playerTotal > Random){
+        else if ( userTotal > Random){
           loser();
         } 
   })  
-  $('.watermellon').on ('click', function(){
-    playerTotal = playerTotal + num3;
-    console.log("New playerTotal= " + playerTotal);
-    $('#totalScore').text(playerTotal);
+$('.watermellon').on ('click', function(){
+  userTotal = userTotal + num3;
+  console.log("New userTotal= " + userTotal);
+    $('#totalScore').text(userTotal);
+          if (userTotal == Random){
+          winner();
+        }
+        else if ( userTotal > Random){
+          loser();
+        } 
+  })  
 
-          if (playerTotal == Random){
-          woohoo();
+$('.pineapple').on ('click', function(){
+    userTotal = userTotal + num4;
+    console.log("New userTotal= " + userTotal);
+    $('#totalScore').text(userTotal); 
+          if (userTotal == Random){
+          winner();
         }
-        else if ( playerTotal > Random){
-          loser();
-        } 
-  })  
-  $('.pineapple').on ('click', function(){
-    playerTotal = playerTotal + num4;
-    console.log("New playerTotal= " + playerTotal);
-    $('#totalScore').text(playerTotal); 
-      
-          if (playerTotal == Random){
-          woohoo();
-        }
-        else if ( playerTotal > Random){
+        else if ( userTotal > Random){
           loser();
         }
   });   
